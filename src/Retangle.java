18147f1fb45aef3a5f9bb0a92fc93c818a7fce68
jenkins <jenkins@ubntu.local>
2018-04-21 17:42:28 -0700

    Java Files

---
+++ src/Retangle.java
@@ -0,0 +1,43 @@
+public class Rectancle {
+
+	public int length;
+	public int width;
+
+
+public Retangle(int lenght, int width){
+
+this.length = length;
+this.width = width;
+}
+
+	public int getArea(){
+
+
+	return length * width;
+}
+
+
+public int getPerimeter(){
+
+return 2 * (length + width);
+
+
+}
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
